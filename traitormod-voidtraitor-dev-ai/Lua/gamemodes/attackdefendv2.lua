ba3df54e34942af225a885d628bda161623e091e
kotofey51 <38777263+sashagavrilyuk@users.noreply.github.com>
2026-08-29 06:50:47 +0200

    Restore AttackDefend balance counter

--- traitormod-voidtraitor-dev-ai/Lua/gamemodes/attackdefendv2.lua
+++ traitormod-voidtraitor-dev-ai/Lua/gamemodes/attackdefendv2.lua
@@ -187,7 +187,7 @@ function gm:_BalanceTeams(newClients)
 			local client = table.remove(priorityTeamMembers, randomPlayerIndex)
 			priorityTeam.Counter = priorityTeam.Counter - 1
 			table.insert(notPriorityTeamMembers, client)
-			priorityTeam.Counter = priorityTeam.Counter + 1
+			notPriorityTeam.Counter = notPriorityTeam.Counter + 1
 		end
 
 		-- Записываем полученные списки в комманды
