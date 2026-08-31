d299db601a2bd1507747ec6e6b3d2d00e13b03ad
kotofey51 <38777263+sashagavrilyuk@users.noreply.github.com>
2026-08-31 05:49:23 +0200

    Revert "Add standard round-end summary"
    This reverts commit e1b33991ac7bbefd0de0dffe9931000770120159.

--- traitormod-voidtraitor-dev-ai/Lua/traitormod.lua
+++ traitormod-voidtraitor-dev-ai/Lua/traitormod.lua
@@ -657,6 +657,8 @@ Hook.Add("missionsEnded", "Traitormod.MissionsEnded", function(missions)
     local endMessage = ""
     if Traitormod.SelectedGamemode then
         endMessage = Traitormod.SelectedGamemode:RoundSummary()
+
+        Traitormod.SendMessageEveryone(Traitormod.HighlightClientNames(endMessage, Color.Red))
     end
     Traitormod.LastRoundSummary = endMessage
 
@@ -1178,7 +1180,6 @@ Traitormod.StringBuilder = dofile(Traitormod.Path .. "/Lua/stringbuilder.lua")
 Traitormod.Voting = dofile(Traitormod.Path .. "/Lua/voting.lua")
 ---@module "Lua.rolemanager"
 Traitormod.RoleManager = dofile(Traitormod.Path .. "/Lua/rolemanager.lua")
-Traitormod.RoundSummaryScreen = dofile(Traitormod.Path .. "/Lua/roundsummary.lua")
 ---@module "Lua.pointshop"
 ---@class Pointshop.Ref: Pointshop
 Traitormod.Pointshop = dofile(Traitormod.Path .. "/Lua/pointshop.lua")
