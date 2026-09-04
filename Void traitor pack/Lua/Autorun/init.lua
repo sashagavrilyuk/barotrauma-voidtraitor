a9ee8cc39c94f0f76af47571ff69c198b0d270f3
kotofey51 <38777263+sashagavrilyuk@users.noreply.github.com>
2026-09-04 22:58:18 +0200

    Load server headset stun hook

--- Void traitor pack/Lua/Autorun/init.lua
+++ Void traitor pack/Lua/Autorun/init.lua
@@ -2,6 +2,9 @@ local packPath = table.pack(...)[1]
 
 assert(loadfile(packPath .. "/Lua/extensions/nt_surgery_access_fix.lua"))(packPath)
 
-if SERVER then return end
+if SERVER then
+    assert(loadfile(packPath .. "/Lua/extensions/headset_stun.lua"))(packPath)
+    return
+end
 
 assert(loadfile(packPath .. "/Lua/client/init.lua"))(packPath)
