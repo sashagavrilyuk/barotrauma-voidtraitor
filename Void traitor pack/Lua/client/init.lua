e93f2dde752d39deb8d3da6275889b295794e5c4
kotofey51 <38777263+sashagavrilyuk@users.noreply.github.com>
2026-09-12 18:04:56 +0200

    Remove AOCS Lua animation loader

--- Void traitor pack/Lua/client/init.lua
+++ Void traitor pack/Lua/client/init.lua
@@ -6,7 +6,6 @@ clientCommon.Language = assert(loadfile(packPath .. "/Lua/language/russian.lua")
 
 assert(loadfile(packPath .. "/Lua/client/welcome.lua"))(packPath, clientCommon)
 assert(loadfile(packPath .. "/Lua/client/roundsummary.lua"))(packPath, clientCommon)
-assert(loadfile(packPath .. "/Lua/client/aocs_trinket_animation.lua"))(packPath)
 assert(loadfile(packPath .. "/Lua/client/spectator/camera_teleport.lua"))(packPath, clientCommon)
 assert(loadfile(packPath .. "/Lua/client/spectator/ghostroles.lua"))(packPath, clientCommon)
 assert(loadfile(packPath .. "/Lua/client/pointshop/gui.lua"))(packPath, clientCommon)
